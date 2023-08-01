3f42f25fa329fc6f164c32e89ddd25f12b60a985
iremnurcolak <iremnurcolak34@gmail.com>
2023-08-01 16:30:49 +0300

    Added dto

---
+++ src/main/java/com/lobemus/shoppingApporderService/service/OrderService.java
@@ -0,0 +1,40 @@
+package com.lobemus.shoppingApporderService.service;
+
+import com.lobemus.shoppingApporderService.dto.OrderLineItemsDto;
+import com.lobemus.shoppingApporderService.dto.OrderRequest;
+import com.lobemus.shoppingApporderService.model.Order;
+import com.lobemus.shoppingApporderService.model.OrderLineItems;
+import com.lobemus.shoppingApporderService.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest)
+    {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItemsList =
+                orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToOrder).toList();
+
+        order.setOrderLineItemsList(orderLineItemsList);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToOrder(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+
+}
